9b5600c19ae8235277b1dfd9695e33bb8d6506f2
Dinhdu2602 <82326575+Dinhdu2602@users.noreply.github.com>
2025-03-07 22:20:59 +0700

    Add files via upload
    Uploads

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Cốm 💘';
+const giftUrl = 'https://www.facebook.com/yTisOggy';
+const eventName = 'Chúc mừng ngày 8-3';
+const titleCard = '💘 Tặng Cốm 💘';
+const contentCard = 'Chúc cậu 8-3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau. Tuy đây chỉ là món quà nhỏ hoi, nhưng mà mong cậu thích nó hihi ^^💘';
+const giftImage = 'Cốm.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
